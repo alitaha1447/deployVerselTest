c60c8f7fca64120f6adbd69712d2aef88745a84c
alitaha1447 <ali.taha1447@gmail.com>
2025-03-20 15:52:24 +0530

    second commit

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.use("/home", (req, res) => {
-  res.send("Welcome, your app is working well in server");
+  res.status(200).json("Welcome, your app is working well in server");
 });
 
 const PORT = 5300;
